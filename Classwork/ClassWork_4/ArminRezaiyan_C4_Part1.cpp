e068a17a2d5f4e7aebd174f260e02d2d452e4d39
ArminRezz <arnojani2005@gmail.com>
2021-10-11 21:01:18 -0400

    classwork4

--- Classwork/ClassWork_4/ArminRezaiyan_C4_Part1.cpp
+++ Classwork/ClassWork_4/ArminRezaiyan_C4_Part1.cpp
@@ -1,7 +1,7 @@
 /*
 * Class: CMSC 140 
 * Instructor: Proffesor Shah
-* Classwork/<2>
+* Classwork/<4>
 * Description: Print out some basic imformation about myself
 * Due Date: 09/23/2021
 * I pledge that I have completed the programming assignment independently. 
@@ -10,13 +10,36 @@
 * Name: Armin Rezaiyan-Nojani
 *
 * Psuedocode:
+** PROGRAM 1
+* Ask user how many tickets sold
+* find subtotal
+* if 0 - 3 print subtotal
+* if 3 - 4 print subtotal x 0.9
+* if 5 - 6 print subtotal x 0.75
+* if 7 - 9 print subtotal x 0.66
+* if 10+ print subtotal x 0.58
+* else - print user needs more tickets sold
 *
+** PROGRAM 2
+* Ask user how many colors, if lighted, if needs removal
+* create price variable float
+* if color is 1 price 250
+* if color is 2 price 325
+* if color is 3 price 450
+* if color is 4 price 625
+* if color is 5+ price 850
+* if lighted price raises 25%
+* if needs removing price raises 10%
+* print final price
 */
 
 #include <iostream>
 using namespace std;
 
 int main() {
+
+
+    // program 1
 
     const int TICKET_PRICE = 109; 
     int ticketsSold = 0;
@@ -43,5 +66,49 @@ int main() {
     else {
         cout << "You must purchase more tickets!"; 
     }
+    
+    // program 2
+
+    int numOfColors = 0;
+    cout << "Enter num of colors in sign? : ";
+    cin >> numOfColors; 
+
+    char lighted; 
+    cout << "Do you want lighted sign? (y/n): ";
+    cin >> lighted;
+
+    char removed; 
+    cout << "Do you want old sign removed? (y/n): ";
+    cin >> removed;
+
+    float price = 0;
+
+    switch (choice) {
+        case 1: 
+            price = 250;
+            break;
+        case 2:
+            price = 325;
+            break;
+        case 3:
+            price = 450;
+            break;
+        case 4:
+            price = 625;
+            break;
+        default:
+            price = 850;
+    }
+
+    if (lighted == 'y') {
+        price *= 1.25; 
+    } 
+
+    if (removed == 'y') {
+        price *= 1.1; 
+    } 
+
+    cout << "The price is " << price; 
+
 
 }
