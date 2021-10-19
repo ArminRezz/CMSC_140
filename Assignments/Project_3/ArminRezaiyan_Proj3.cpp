cdf8a733cd15dbddc9ae3683755cafbecb365e87
ArminR <arnojani2005@gmail.com>
2021-10-19 10:10:03 -0400

    random commit

---
+++ Assignments/Project_3/ArminRezaiyan_Proj3.cpp
@@ -0,0 +1,25 @@
+/*
+* Class: CMSC 140 
+* Instructor: Proffesor Shah
+* Project/<2>
+* Description: Express given age in many different units and demonstrate the operators in c++
+* Due Date: 10/11/2021
+* I pledge that I have completed the programming assignment independently. 
+* I have not copied the code from a student or any source
+* I have not given my code to any student
+* Name: Armin Rezaiyan-Nojani 
+*
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <math.h> 
+using namespace std;
+
+int main() {
+
+    
+
+    return 0;
+}
