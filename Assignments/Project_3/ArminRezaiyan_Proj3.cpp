2333285dd47a1d3bf9742d65c652ce259d483e22
ArminR <arnojani2005@gmail.com>
2021-10-20 21:03:05 -0400

    commit 50%_proj3_mac

--- Assignments/Project_3/ArminRezaiyan_Proj3.cpp
+++ Assignments/Project_3/ArminRezaiyan_Proj3.cpp
@@ -19,7 +19,69 @@ using namespace std;
 
 int main() {
 
-    const SINGLE_RATE =  
+    const int SINGLE_RATE = 60;   
+    const int DOUBLE_RATE = 75; 
+    const int KING_RATE = 100; 
+    const int SUITE_RATE = 150; 
+    const int MIN_FLOORS = 1;
+    const int MAX_FLOORS = 5;
+    const int MIN_ROOMS = 1;
+    const int MAX_ROOMS = 30;
+
+    string typeOfRoom[4] = {"SINGLE", "DOUBLE", "KING", "SUITE"};
+    int numOfFloors = 0; 
+    int roomsPerFloor[4]; 
+    int occupiedRoomsPerFloor[4]; 
+    string location;
+
+    cout << "Enter location of hotel: " << endl;
+    getline(cin, location);
+
+    
+    do { 
+        cout << "Enter number of floors: " << endl;
+        cin >> numOfFloors; 
+    } while (numOfFloors < MIN_FLOORS || numOfFloors > MAX_FLOORS);
+
+    
+    for (int x = 1; x <= numOfFloors; x++) {
+        do {
+            cout << "Enter number of rooms on floor #" << x << ": " << endl; 
+            cin >> roomsPerFloor[x-1]; 
+
+            if (roomsPerFloor[x-1] < MIN_ROOMS || roomsPerFloor[x-1] > MAX_ROOMS) {
+                cout << "Number of rooms should be between 1 and 30! Try Again!\n" << endl; 
+            } 
+        } while (numOfRooms < MIN_ROOMS || numOfRooms > MAX_ROOMS);
+
+        do {
+            int temp[4]; 
+            for (int i = 0; i < 4; i++) {
+                cout << "How many " << typeOfRoom[i] + "rooms are occupied on floor #" << x << ": " << endl; 
+                cin >> temp[i];
+                occupiedRoomsPerFloor[x-1] += temp[i]; 
+            }
+            
+            if (occupiedRoomsPerFloor[x-1] > MAX_ROOMS) {
+                cout << "Total number of occupied rooms exceeds the number of available rooms on this floor! Try Again!" << endl; 
+            } 
+        } while (occupiedRoomsPerFloor[x-1] > MAX_ROOMS);
+    }
+
+    cout << "- - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -" << endl; 
+    cout << "- - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -" << endl; 
+    cout << setw(20) << right << "BlueMont Hotel located in " << location << endl; 
+    cout << "- - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -" << endl; 
+    cout << "- - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -" << endl; 
+
+    cout << setw(20) << right << "Today's Room Rates(US$/night)" << endl;
+    cout << setw(10) << right << "Single Room" << setw(10) << "Double Room" <<  setw(10) << "King Room" << setw(10) << "Suite Room" << endl; 
+    cout << setw(10) << right << "Single Room" << setw(10) << "Double Room" <<  setw(10) << "King Room" << setw(10) << "Suite Room" << endl; 
+
+    cout << "- - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -" << endl; 
+    cout << "- - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -  - - - - - - - - - -" << endl; 
+
+    cout <<  
 
 
 
